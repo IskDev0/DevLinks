ab104c7436e050bb1396580100a2da12aef7f516
Iskander <isk.dev001@gmail.com>
2023-10-11 15:47:02 +0600

    Removed unnecessary css class

--- components/ProfileShareSkeleton.vue
+++ components/ProfileShareSkeleton.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-[400px] mx-auto flex flex-col items-center gap-4 bg-white px-4 py-8 rounded-xl mt-32">
+  <div class="w-[400px] mx-auto flex flex-col items-center gap-4 bg-white px-4 py-8 rounded-xl">
     <div class="flex flex-col items-center gap-4 w-full animate-pulse">
       <div class="h-32 w-32 bg-gray-200 rounded-full"></div>
       <div class="h-6 bg-gray-200 rounded-full w-56"></div>
